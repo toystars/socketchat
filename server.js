2849fa8cd8e708baaa3ff99aac62ab483ebbeeb4
andela-bmustapha <mustapha.babtunde@andela.co>
2015-10-04 22:31:01 +0100

    Implements basic android Socket connection and event listener

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ io.on('connection', function(client) {
   console.log('Client connected...');
   client.on('join', function(data) {
     console.log(data);
-    client.emit('messages', 'Hello from server');
+    client.emit('messages', data);
   });
 });
 
